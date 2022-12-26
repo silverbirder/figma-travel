3f4e332c9921fad4be980a21bdb4567247687be7
silverbirder <silverbirder@gmail.com>
2022-12-26 21:22:16 +0900

    change filter

--- src/find.mjs
+++ src/find.mjs
@@ -11,9 +11,32 @@ export const findTypeIsText = (data) => {
 };
 
 export const filterFont = (data) => {
+  /*
+              "style": {
+              "fontFamily": "Noto Sans JP",
+              "fontPostScriptName": "NotoSansJP-Regular",
+              "fontWeight": 400,
+              "textAutoResize": "WIDTH_AND_HEIGHT",
+              "fontSize": 13,
+              "textAlignHorizontal": "LEFT",
+              "textAlignVertical": "TOP",
+              "letterSpacing": 0,
+              "lineHeightPx": 24,
+              "lineHeightPercent": 127.49681091308594,
+              "lineHeightPercentFontSize": 184.61538696289062,
+              "lineHeightUnit": "PIXELS"
+            },
+  */
   return data.filter((d) => {
     const {
-      style: { fontWeight, fontSize },
+      style: {
+        fontFamily,
+        fontWeight,
+        fontSize,
+        textAlignHorizontal,
+        letterSpacing,
+        lineHeightPx,
+      },
       fills: [
         {
           color: { r, g, b },
@@ -21,7 +44,15 @@ export const filterFont = (data) => {
       ],
     } = d;
     return (
-      fontSize === 12 && fontWeight === 400 && r === 0 && g === 0 && b === 0
+      fontFamily === "Inter" &&
+      fontWeight === 400 &&
+      fontSize === 12 &&
+      textAlignHorizontal == "LEFT" &&
+      letterSpacing === 0 &&
+      lineHeightPx === 16
+      // r === 0 &&
+      // g === 0 &&
+      // b === 0
     );
   });
 };
